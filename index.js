dc221f0513127b1a638b18b233460884f70bd6a6
amon-boss <66650142+amon-boss@users.noreply.github.com>
2025-07-01 12:37:20 +0000

    Update index.js

--- index.js
+++ index.js
@@ -11,15 +11,14 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-.then(() => console.log('鉁� Connect茅 脿 MongoDB'))
-.catch((err) => console.error('鉂� Erreur MongoDB:', err));
+.then(() => console.log('✅ Connecté à MongoDB'))
+.catch((err) => console.error('❌ Erreur MongoDB:', err));
 
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/services', require('./routes/services'));
 app.use('/api/users', require('./routes/user'));
-app.use('/api/messages', require('./routes/messages'));
 
-app.get('/', (req, res) => res.send('Service Connect API 馃殌'));
+app.get('/', (req, res) => res.send('Service Connect API 🚀'));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log('馃殌 Serveur lanc茅 sur le port', PORT))
+app.listen(PORT, () => console.log('🚀 Serveur lancé sur le port', PORT));
